cd300b994710717022b74b91a4cad696c8962c7d
MeghanaJammu <jammumeghana749@gmail.com>
2025-09-23 13:29:58 +0530

    testing pipeline flow after automating with gcp cloud VM and webhooks connection third time, after making performance optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { lazy, Suspense } from "react"; // to impove performace of app,im
 import Navbar from "./components/Navbar";
 import { useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import Loader from "./components/Loader";
 
 // Converting the static import to a lazy import
 const AppRoutes = lazy(() => import("./routes/AppRoutes"));
@@ -15,7 +14,13 @@ const App = () => {
     <div className="bg-[#0f172a] text-[#e2e8f0] min-h-screen">
       {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
 
-      <Suspense fallback={<Loader />}>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            Loading...
+          </div>
+        }
+      >
         <AppRoutes />
       </Suspense>
 
